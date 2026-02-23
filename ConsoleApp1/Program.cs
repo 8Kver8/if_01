c341f4a218696d2fff54c84042875729c5766990
Kirill Verkhozin <kirill.verkhozin@example.com>
2026-02-23 16:47:38 +0800

    декомпозиция

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,28 +1,38 @@
 ﻿using System;
 
-class Program
+namespace FirstApp
 {
-    static void Main()
+    public class Logic
     {
-        Console.Write("введите двузначное число: ");
-        int num = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("введите цифру: ");
-        int c = Convert.ToInt32(Console.ReadLine());
-
-        int a = num / 10;
-        int b = num % 10;
-        string result;
-
-        if (a == c || b == c)
+        public static string CheckDigit(int num, int c)
         {
-            result = "такая цифра есть";
-        }
-        else
-        {
-            result = "такой цифры нет";
-        }
+            int a = num / 10;
+            int b = num % 10;
 
-        Console.WriteLine(result);
+            if (a == c || b == c)
+            {
+                return "такая цифра есть";
+            }
+            else
+            {
+                return "такой цифры нет";
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("введите двузначное число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("введите цифру: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            string result = Logic.CheckDigit(num, c);
+
+            Console.WriteLine(result);
+        }
     }
 }
