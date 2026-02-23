099cb7137be564a12e77ecc81d32a996c6bc0b0c
Kirill Verkhozin <kirill.verkhozin@example.com>
2026-02-23 16:19:27 +0800

    первая версия

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("введите двузначное число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("введите цифру: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        int a = num / 10;
+        int b = num % 10;
+
+        if (a == c || b == c) 
+        {
+            Console.WriteLine("такая цифра есть");
+        }
+        else 
+        {
+            Console.WriteLine("такой цифры нет");
+        }
+    }
+}
